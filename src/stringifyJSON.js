3cd1ea3134486c33c5312202598d0f0a8addbc28
jon-zawada <jonathan_zawada@yahoo.com>
2020-01-20 15:19:07 -0800

    Account for all scalar values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var resultStr = ''
+  //numbers
+  if(typeof obj === 'number'){
+    resultStr+= obj
+  }
+
+  //null
+  if(obj === null){
+    return 'null'
+  }
+
+  //boolean
+  if(typeof obj === 'boolean'){
+    resultStr+= obj
+  }
+
+  //already string
+  if(typeof obj === 'string'){
+    var strObj = '"' + obj + '"'
+    resultStr+= strObj
+  }
+
+  if(Array.isArray(obj) && obj.length === 0){
+    return '[]'
+  }
+
+  //for array with elements
+
+  //for type object
+
+  return resultStr
 };
